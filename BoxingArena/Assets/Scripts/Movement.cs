e1f8cf32d9ba1a5e447bcadcc5cac7d214dd46b6
Josh Brackett <joshua.brackett@hotmail.ca>
2018-03-07 02:13:52 -0500

    fixed rotation and added 2 ways to move the player to see which one we like better

--- BoxingArena/Assets/Scripts/Movement.cs
+++ BoxingArena/Assets/Scripts/Movement.cs
@@ -6,27 +6,43 @@ public class Movement : MonoBehaviour {
 
 	public float m_speed = 5.0f;
 
-	private Vector3 m_movementX;
-	private Vector3 m_movementZ;
 	private Rigidbody m_rb;
-	void Start () {
+	private float m_velocitySlowAmount = 0.8f;
+
+	void Start() {
 		m_rb = GetComponent<Rigidbody>();
 	}
+
+	 void Update() {
+		Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+		Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+		float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+		transform.rotation = Quaternion.Euler(new Vector3(0, -angle, 0));
+    }
 	
 	void FixedUpdate() {
+		// mouse location is always forward
 		if(Input.GetKey(KeyCode.W)) {
-	 		m_movementZ = Vector3.forward * m_speed; 
+	 		m_rb.velocity = transform.forward * m_speed; 
 		}
 		if(Input.GetKey(KeyCode.S)) {
-			m_movementZ = Vector3.back * m_speed; 
+			m_rb.velocity = -transform.forward * m_speed; 
 		}
 		if(Input.GetKey(KeyCode.A)) {
-			m_movementX = Vector3.left * m_speed; 
+			m_rb.velocity = -transform.right * m_speed; 
 		}
 		if(Input.GetKey(KeyCode.D)) {
-			m_movementX = Vector3.right * m_speed; 
+			m_rb.velocity = transform.right * m_speed; 
 		}
 
-		m_rb.velocity = new Vector3(m_movementX.x, 0, m_movementZ.z);
+		// slows the velocity over time if nothing else is preesed
+		m_rb.velocity = m_rb.velocity * m_velocitySlowAmount;
+
+		// top if the screen is always forward rotation
+		// m_rb.velocity = new Vector3(Mathf.Lerp(0, Input.GetAxis("Vertical") * m_speed, 0.8f), 0, Mathf.Lerp(0, Input.GetAxis("Horizontal") * m_speed, 0.8f));
 	}
+
+	float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
 }
